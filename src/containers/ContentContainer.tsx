621d3eed477881055486ba250d15bc4e02cae643
Caleb Tracey <caleb.tracey@gmail.com>
2023-02-08 22:41:32 -0500

    more ui development

--- src/containers/ContentContainer.tsx
+++ src/containers/ContentContainer.tsx
@@ -2,23 +2,12 @@ import { Container } from 'react-bootstrap'
 import useSearch, { initialSearchState } from '../hooks/useSearch'
 import SearchBar from '../components/SearchBar'
 import Header from '../components/header/Header'
-import useService, { initialServiceState } from '../hooks/useService'
 import Results from '../components/results/Results'
-import { useState } from 'react'
 
 export const ContentContainer = (): JSX.Element => {
   const [searchBarState, handler] = useSearch(initialSearchState)
-  const [serviceState, serviceHandler] = useService(initialServiceState)
-  // const [respCount, setRespCount] = useState(0)
 
-  // const [searchBarLoc, setSearchBarLoc] = useState<SearchBarLocT>('center')
-  const { location, searchLoading, prompt } = searchBarState
-
-  // useEffect(() => {
-  //   if (result) {
-  //     setRespCount(result.data.length)
-  //   }
-  // }, [result])
+  const { location } = searchBarState
 
   return (
     <div className='content-container'>
--- src/services/Service.tsx
+++ src/services/Service.tsx
@@ -1,10 +1,28 @@
-import axios, { isAxiosError } from 'axios'
+import axios, { AxiosError, isAxiosError } from 'axios'
 import Constants from '../Constants'
 
+const { LocalURL, LocalTestURL, DevURL } = Constants
 interface Props {
   request: RequestI
 }
 
+const newErrorResponse = (cause: string): ResponseI => {
+  return {
+    result: { created: 0, data: [] },
+    message: {
+      errorLog: [
+        {
+          scope: 'Service',
+          statusCode: '500',
+          rootCause: cause,
+          trace: 'Post: error',
+        },
+      ],
+      status: 'ERROR',
+      count: '0',
+    },
+  }
+}
 const PostError: ResponseI = {
   result: { created: 0, data: [] },
   message: {
@@ -21,8 +39,7 @@ const PostError: ResponseI = {
   },
 }
 
-const GetImages = async (props: Props): Promise<ResponseI | null> => {
-  const { LocalURL, LocalTestURL, DevURL } = Constants
+const GetImages = async (props: Props): Promise<ResponseI> => {
   const { request } = props
   try {
     const response = await axios.post<ResponseI>(DevURL, request)
@@ -30,11 +47,11 @@ const GetImages = async (props: Props): Promise<ResponseI | null> => {
   } catch (error) {
     if (isAxiosError(error)) {
       console.error(error.message)
+      return newErrorResponse(error.cause ? error.cause.message : error.message)
     } else {
       console.error(error)
+      return newErrorResponse(JSON.stringify(error))
     }
-
-    return null
   }
 }
 
